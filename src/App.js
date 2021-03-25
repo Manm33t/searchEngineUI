9b2c3b4bd09feab5dcb9e2ca1b9905111e9cb3fd
Manmeet Singh <manmeet.singh@medtrail.in>
2021-03-25 19:08:54 +0530

    added  multi select with search  functionality

--- src/App.js
+++ src/App.js
@@ -1,29 +1,44 @@
 import React, {Component} from 'react';
-import styles from './style.less';
+import styles from './style.css';
+
+import AsyncSelect from 'react-select/async';
+
 
 class App extends Component {
   state = {
     searchText: undefined,
     searchData: [],
+    selectedData: [],
     item: {},
     showDropDown: false
   };
-  handleSearch = (search) => {
-    const {value} = search.target;
+
+  getShortName = (name = '', length = 6) => {
+    let str = name;
+    if (name.length > length) {
+      str = name.substr(0, length) + '...';
+    }
+    return str
+  }
+  handleSearch = (search, callback) => {
+    console.log(search);
+    // const {value} = search.target;
     const {searchData} = this.state;
     const updatedSearchData = searchData;
-    updatedSearchData.push({name: value});
-    console.log(value);
-    this.setState({searchText: value, searchData: updatedSearchData}, () => {
+    updatedSearchData.push({name: search});
+    console.log(search);
+    this.setState({searchText: search, searchData: updatedSearchData}, () => {
       const requestOptions = {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({searchText: value})
+        body: JSON.stringify({searchText: search})
       };
       fetch('http://localhost:7000/search', requestOptions).then(res => res.json()).then(json => {
 
         this.setState({searchData: json.data, showDropDown: json.data.length});
+        callback(json.data.map(i => ({label: i.Title, value: i.ID})))
       })
+
     })
   };
   handleSearchClick = (a) => {
@@ -39,55 +54,65 @@ class App extends Component {
     })
   };
 
+  handleChange = (selectedUsers = []) => {
+    console.log(selectedUsers);
+    const updatedSelectedUsers = selectedUsers.map(a => ({label: this.getShortName(a.label,10), value: a.value}));
+    console.log(updatedSelectedUsers, 'updated')
+    this.setState({selectedData: updatedSelectedUsers || []});
+  };
+
+
   render() {
-    const {searchText, searchData, item: {Title, Sponsor}, showDropDown} = this.state;
-    // console.log(styles, 'styles');
+    const {searchText, selectedData, searchData, item: {Title, Sponsor}, showDropDown} = this.state;
+    console.log(selectedData, searchData, 'styles');
     return (
       <React.Fragment>
         <div>
-          <h1 className={styles.header}>Custom Search Engine</h1>
-          <div className={styles.mainDiv}>
-            <input
-              type='text'
-              value={searchText}
-              onChange={this.handleSearch}
-              onBlur={this.handleSearch}
+          <h1 className="header">Custom Search Engine</h1>
+          <div className="mainDiv">
+            <AsyncSelect className="AsyncSelect"
+                         isMulti
+                         value={selectedData}
+                         onChange={this.handleChange}
+                         loadOptions={this.handleSearch}
+                         placeHolder='type something...'
+                         closeMenuOnSelect={false}
             />
           </div>
-          <div className={styles.list} style={{display: `${showDropDown ? 'initial' : 'none'}`}}>
-            {searchData.map(a => {
-              return (
-                <ul
-                  style={{color: a.highlight ? 'lightblue' : 'white'}}
-                  onClick={() => this.handleSearchClick(a)}
-                >
-                  {a.Title}
-                </ul>
-              )
-            })}
+          {/*<div className="list" style={{display: `${showDropDown ? 'initial' : 'none'}`}}>*/}
+          {/*  {searchData.map(a => {*/}
+          {/*    return (*/}
+          {/*      <ul*/}
+          {/*        style={{color: a.highlight ? 'lightblue' : 'white'}}*/}
+          {/*        onClick={() => this.handleSearchClick(a)}*/}
+          {/*      >*/}
+          {/*        {a.Title}*/}
+          {/*      </ul>*/}
+          {/*    )*/}
+          {/*  })}*/}
 
-          </div>
-          {Title ?
-            <div className={styles.title}>
-              <div>
-                <span id='1'>
-                  Sponsor
-                </span>
-                <span id='2'>
-                 : {Sponsor}
-                </span>
+          {/*</div>*/}
+          {/*{Title ?*/}
+          {/*  <div className="title">*/}
+          {/*    <div>*/}
+          {/*      <span id='1'>*/}
+          {/*        Sponsor*/}
+          {/*      </span>*/}
+          {/*      <span id='2'>*/}
+          {/*       : {Sponsor}*/}
+          {/*      </span>*/}
 
-              </div>
-              <div>
-                <span id='1'>
-                  Title
-                </span>
-                <span id='2'>
-                : {Title}
-                </span>
+          {/*    </div>*/}
+          {/*    <div>*/}
+          {/*      <span id='1'>*/}
+          {/*        Title*/}
+          {/*      </span>*/}
+          {/*      <span id='2'>*/}
+          {/*      : {Title}*/}
+          {/*      </span>*/}
 
-              </div>
-            </div> : null}
+          {/*    </div>*/}
+          {/*  </div> : null}*/}
 
         </div>
 
